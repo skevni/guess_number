5e77887c94dc535b9651e0e3e9d69d20c0d9950d
SklyarovE.N. <skevni@yandex.ru>
2025-05-05 00:17:06 +0300

    optimize key logic

--- gess_number.py
+++ gess_number.py
@@ -12,9 +12,9 @@ while True:
     # print(number)
     if guess > number:
         print('Ваше число меньше того, что загадано.')
-    if guess < number:
+    elif guess < number:
         print('Ваше число больше того, что загадано.')
-    if guess == number:
+    elif guess == number:
         break
 
 print('Отличная интуиция! Вы угадали число :)')
